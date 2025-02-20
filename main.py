c3b1f7b17990f903a367dc7ec4544595dad2b619
nextpassword99 <inderscop@gmail.com>
2025-02-20 00:00:45 -0500

    feat: Usar   `face_recognition` y cv2 para detectar y mostrar rostros en vivo

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import face_recognition
+import cv2
 
+video_capture = cv2.VideoCapture(0)
 
-image = face_recognition.load_image_file("public/p.jpg")
-face_locations = face_recognition.face_locations(image)
+while True:
+    ret, frame = video_capture.read()
+    face_locations = face_recognition.face_locations(frame)
+    
+    if face_locations:
+        print(face_locations)
 
-print(face_locations)
+    for top, right, bottom, left in face_locations:
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+
+cv2.destroyAllWindows()
