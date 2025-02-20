85f92413e3627b8732259f46dcfb863c4cb9680c
nextpassword99 <inderscop@gmail.com>
2025-02-20 17:36:36 -0500

    feat: Añadir métodos para guardar y recuperar rostros en la base de datos

--- app/config/Database.py
+++ app/config/Database.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
 from typing import Dict, Any, Optional
+import bson
+from datetime import datetime
+import numpy as np
 
 
 class Database:
-    def __init__(self, uri: str = "mongodb://localhost:27017", db_name: str = "pictures"):
+    def __init__(self, uri: str = "mongodb://localhost:27017", db_name: str = "faces"):
         self.uri = uri
         self.db_name = db_name
         self.client = None
@@ -30,3 +33,22 @@ class Database:
         result = collection.update_one(query, {"$set": new_values})
 
         return result.modified_count
+
+    def save_face(self, user_id: str, face_encoding: list, face_image_binary: bytes) -> str:
+        face_encoding_list = face_encoding.tolist() if isinstance(
+            face_encoding, np.ndarray) else face_encoding
+
+        face_document = {
+            "user_id": user_id,
+            "face_encoding": face_encoding_list,
+            "face_image": bson.Binary(face_image_binary),
+            "timestamp": datetime.now()
+        }
+
+        return self.insertDocument("faces", face_document)
+
+    def get_user_by_face_encoding(self, face_encoding: list) -> Optional[Dict[str, Any]]:
+        return self.findDocument("faces", {"face_encoding": face_encoding})
+
+    def get_all_users(self):
+        return list(self.db["faces"].find())
