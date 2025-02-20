8c7b72418d192dab70c00a050542568d49386ba1
nextpassword99 <inderscop@gmail.com>
2025-02-20 15:51:35 -0500

    refactor: Modificar `main.py` para inicializar la clase Detector eliminar lógica de detección

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-import face_recognition
-import cv2
+from app.src.Detector import Detector
 
-video_capture = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = video_capture.read()
-    face_locations = face_recognition.face_locations(frame)
-    
-    if face_locations:
-        print(face_locations)
-
-    for top, right, bottom, left in face_locations:
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-video_capture.release()
-
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    Detector()
