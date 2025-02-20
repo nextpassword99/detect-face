bf6536baf1f95e7a0cf7e373a6cb30dedcc65b25
nextpassword99 <inderscop@gmail.com>
2025-02-20 15:50:31 -0500

    refactor: Modificar métodos de captura de video y mostrar para mejorar el manejo de la cámara

--- app/src/Detector.py
+++ app/src/Detector.py
@@ -4,23 +4,43 @@ import cv2
 
 class Detector:
     def __init__(self):
+        self._initializeCamera()
+
         while True:
-            frame = self._videoCapture()
-            face_locations = self._faceLocations(frame)
-            self._showVideo(frame, face_locations)
+            frame = self._captureFrame()
+            if frame is not None:
+                face_locations = self._faceLocations(frame)
+                self._displayVideo(frame, face_locations)
+            else:
+                print('No frame')
 
-    def _videoCapture(self):
-        video_capture = cv2.VideoCapture(0)
-        ret, frame = video_capture.read()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
+        self._releaseResources()
+
+    def _initializeCamera(self):
+        self.video_capture = cv2.VideoCapture(0)
+        if not self.video_capture.isOpened():
+            print("Error: No se pudo abrir la cámara.")
+            exit(1)
+
+    def _captureFrame(self):
+        ret, frame = self.video_capture.read()
+        if not ret:
+            return None
         return frame
 
     def _faceLocations(self, frame):
-        face_locations = face_recognition.face_locations(frame)
-        return face_locations
+        return face_recognition.face_locations(frame)
 
-    def _showVideo(self, frame, face_locations):
+    def _displayVideo(self, frame, face_locations):
         for top, right, bottom, left in face_locations:
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
 
         cv2.imshow('Video', frame)
+
+    def _releaseResources(self):
+        if self.video_capture is not None:
+            self.video_capture.release()
+        cv2.destroyAllWindows()
