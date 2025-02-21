643ca772725aed61f71c52ee768b0fdbfbe6f1ee
nextpassword99 <inderscop@gmail.com>
2025-02-21 06:23:38 -0500

    feat: Añadir método para guardar el rostro en la base de datos

--- app/services/Storage.py
+++ app/services/Storage.py
@@ -5,3 +5,16 @@ from datetime import datetime
 import numpy as np
 
 class Storage (Database):
+    def save_face(self, user_id: str, face_encoding: list, face_image_binary: bytes) -> str:
+        face_encoding_list = face_encoding.tolist() if isinstance(
+            face_encoding, np.ndarray) else face_encoding
+
+        face_document = {
+            "user_id": user_id,
+            "face_encoding": face_encoding_list,
+            "face_image": bson.Binary(face_image_binary),
+            "timestamp": datetime.now()
+        }
+
+        return self.insertDocument("faces", face_document)
+    
