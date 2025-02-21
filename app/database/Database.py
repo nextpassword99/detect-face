89910ee7313e15e69657f5527035949dd3ab6e59
nextpassword99 <inderscop@gmail.com>
2025-02-21 06:25:45 -0500

    feat: Eliminar métodos obsoletos de la clase Database

--- app/database/Database.py
+++ app/database/Database.py
@@ -1,8 +1,5 @@
 from pymongo import MongoClient
 from typing import Dict, Any, Optional
-import bson
-from datetime import datetime
-import numpy as np
 
 
 class Database:
@@ -33,22 +30,3 @@ class Database:
         result = collection.update_one(query, {"$set": new_values})
 
         return result.modified_count
-
-    def save_face(self, user_id: str, face_encoding: list, face_image_binary: bytes) -> str:
-        face_encoding_list = face_encoding.tolist() if isinstance(
-            face_encoding, np.ndarray) else face_encoding
-
-        face_document = {
-            "user_id": user_id,
-            "face_encoding": face_encoding_list,
-            "face_image": bson.Binary(face_image_binary),
-            "timestamp": datetime.now()
-        }
-
-        return self.insertDocument("faces", face_document)
-
-    def get_user_by_face_encoding(self, face_encoding: list) -> Optional[Dict[str, Any]]:
-        return self.findDocument("faces", {"face_encoding": face_encoding})
-
-    def get_all_users(self):
-        return list(self.db["faces"].find())
